765b8dd167d94e40f1c566cf862ce6936c19e7da
vijaybailapudi <159618659+vijaybailapudi@users.noreply.github.com>
2025-11-22 23:58:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+/* 
+1. Use the inquirer npm package to get user input.
+2. Use the qr-image npm package to turn the user entered URL into a QR code image.
+3. Create a txt file to save the user input using the native fs node module.
+*/
+
+import inquirer from "inquirer";
+import qr from "qr-image";
+// var qr = require('qr-image');
+import fs from "fs";
+
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        message : 'type your url',
+        name : "URL"
+    }
+  ])
+
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_img.png"));
+
+    fs.writeFile("url.txt",url,(err)=>{
+        if (err) throw err;
+        console.log('the url text file is saved');
+    })
+  })
+
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
